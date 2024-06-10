a0b045b488962428c49c5a4892c9aeff30f1a585
Raghu Rayamajhi <ryamacoding@outlook.com>
2024-06-10 12:24:57 +0100

    working on thumbnails now

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -3,6 +3,8 @@ import { Movie } from "../typings"
 import { useEffect, useState } from "react"
 import { baseUrl } from "../constants/movie"
 import Image from "next/image"
+import { FaPlay } from "react-icons/fa"
+import { InformationCircleIcon } from "@heroicons/react/24/solid"
 
 interface Props {
     netflixOriginals: Movie[]
@@ -29,11 +31,11 @@ function Banner({ netflixOriginals }: Props) {
             </div>
 
             <h1 className="text-2xl lg:text-7xl md:text-7xl font-bold">{movie?.title || movie?.name || movie?.original_name}</h1>
-            <p className="max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">{movie?.overview}</p>
+            <p className="max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">{movie?.overview}</p>
 
-            <div>
-                <button>Play</button>
-                <button>More Info</button>
+            <div className="flex space-x-3">
+                <button className="bannerButton bg-white text-black"><FaPlay className="h-4 w-4 text-black md:h-7 md:w-7" />Play</button>
+                <button className="bannerButton bg-[gray]/70">More Info <InformationCircleIcon className="h-5 w-5 md:h-8 md:w-8" /></button>
             </div>
         </div>
     )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Header from "../../components/Header";
 import Banner from "../../components/Banner";
 import requests from "../../utils/request";
 import { Movie } from "../../typings";
+import Row from "../../components/Row";
 
 interface Props {
   netflixOriginals: Movie[]
@@ -16,7 +17,7 @@ interface Props {
 }
 
 
-const Home = ({   
+const Home = ({
   netflixOriginals,
   actionMovies,
   comedyMovies,
@@ -25,7 +26,7 @@ const Home = ({
   romanceMovies,
   topRated,
   trendingNow,
- }: Props) => {
+}: Props) => {
   return (
     <div
       className="relative h-screen bg-gradient-to-b from-gray-900/10
@@ -35,16 +36,18 @@ const Home = ({
         <title>Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header/>
-      <main>
-        <Banner netflixOriginals={netflixOriginals}/>
+      <Header />
+      <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
+        <Banner netflixOriginals={netflixOriginals} />
         <section>
-          {/* row */}
-          {/* row */}
-          {/* row */}
-          {/* row */}
-          {/* row */}
-          {/* row */}
+          <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
+          {/* My List Component */}
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
         </section>
       </main>
       {/* Modal */}
@@ -75,7 +78,7 @@ export const getServerSideProps = async () => {
     fetch(requests.fetchDocumentaries).then((res) => res.json()),
   ])
 
-  return{
+  return {
     props: {
       netflixOriginals: netflixOriginals.results,
       trendingNow: trendingNow.results,
