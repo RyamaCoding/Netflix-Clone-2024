0912c0c73f5863574dcc7d72f744f682fc47c423
Raghu Rayamajhi <ryamacoding@outlook.com>
2024-06-11 14:48:06 +0100

    fixing prettier formatting issues and completing login authentication

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -37,13 +37,32 @@ export const AuthProvider = ({children}: AuthProviderProps) => {
     const [loading, setLoading] = useState(false)
     const [user, setUser] = useState<User | null>(null)
     const [error, setError] = useState(null)
+    const [initialLoading, setInitialLoading] = useState(true)
     const router = useRouter()
+
+    useEffect(
+        () =>
+          onAuthStateChanged(auth, (user) => {
+            if (user) {
+              // Logged in...
+              setUser(user)
+              setLoading(false)
+            } else {
+              // Not logged in...
+              setUser(null)
+              setLoading(true)
+              router.push('/login')
+            }
+    
+            setInitialLoading(false)
+          }),
+        [auth]
+      )
 
     const signUp = async (email: string, password: string) => {
         setLoading(true)
 
-        await createUserWithEmailAndPassword(auth, email, password).then((userCredential)
-            => {
+        await createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
             setUser(userCredential.user)
             router.push('/')
             setLoading(false)
@@ -55,8 +74,7 @@ export const AuthProvider = ({children}: AuthProviderProps) => {
     const signIn = async (email: string, password: string) => {
         setLoading(true)
 
-        await signInWithEmailAndPassword(auth, email, password).then((userCredential)
-            => {
+        await signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
             setUser(userCredential.user)
             router.push('/')
             setLoading(false)
@@ -84,7 +102,7 @@ export const AuthProvider = ({children}: AuthProviderProps) => {
     }), [user, loading])
 
     return (<AuthContext.Provider value={memoedValue}>
-        {children}
+        {!initialLoading && children}
     </AuthContext.Provider>)
 
 }
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,6 +2,7 @@ import Head from "next/head"
 import Image from "next/image"
 import { useState } from "react"
 import { SubmitHandler, useForm } from "react-hook-form";
+import useAuth from "../../hooks/useAuth";
 
 interface Inputs {
   email: string;
@@ -11,15 +12,16 @@ interface Inputs {
 function Login() {
 
   const [login, setLogin] = useState(false)
+  const {signIn, signUp} = useAuth()
 
   const { register, handleSubmit, formState: { errors } }
-    = useForm<Inputs>();
+    = useForm<Inputs>()
   const onSubmit: SubmitHandler<Inputs> = async ({email, password}) => {
     if (login) {
-      // await signIn(email, password)
+      await signIn(email, password)
     }
     else {
-      // await signUp(email, password)
+      await signUp(email, password)
     }
   }
 
